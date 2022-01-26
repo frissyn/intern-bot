606561f1fa79f05a0e00561e22485bd82112379e
frissyn <62220201+frissyn@users.noreply.github.com>
2022-01-26 16:52:47 +0000

    :sparkles: add source initialization

---
+++ source/__init__.py
@@ -0,0 +1,25 @@
+import os
+import nextcord
+
+from nextcord.ext import commands
+
+COLOR = 0xF2DB94
+TOKEN = os.environ["TOKEN"]
+INTENTS = nextcord.Intents.all()
+
+robot = commands.Bot(command_prefix="%", intents=INTENTS)
+extensions = {
+    "events": ["on_ready"],
+    "cogs": [
+        "clashes",
+        "dump",
+        "sandbox",
+        "starboard",
+        "utility"
+    ]
+}
+
+
+for key, values in extensions.items():
+    for target in values:
+        robot.load_extension(f"source.{key}.{target}")
