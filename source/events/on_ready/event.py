64f08e29d29eb225490522e32418a7a29efa82f2
frissyn <62220201+frissyn@users.noreply.github.com>
2022-02-01 12:57:35 +0000

    :adhesive_bandage: move status check to on_connect event

--- source/events/on_ready/event.py
+++ source/events/on_ready/event.py
@@ -5,9 +5,5 @@ from source import robot
 
 @robot.event
 async def on_ready():
-    robot.remove_command("help")
-
-    await robot.change_presence(status=nextcord.Status.dnd)
-
     print(f"{robot.user.name} has connected to Discord.")
     print(f"Current Event Loop: {robot.loop}")
